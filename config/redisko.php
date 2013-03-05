a2d5f4ef43fca9988e1930446c0c46de89292e0c
Eric Liang <etlgfx@gmail.com>
2013-03-05 14:52:18 -0500

    removing default IGBINARY serializer as it's a dependency that isn't needed

--- config/redisko.php
+++ config/redisko.php
@@ -5,7 +5,7 @@ return array(
 	'default' => array(
 		'host'       => 'localhost',
 		'port'       => 6379,
-		'serializer' => Redis::SERIALIZER_IGBINARY, // SERIALIZER_NONE, SERIALIZER_PHP, SERIALIZER_IGBINARY
+		'serializer' => Redis::SERIALIZER_NONE, //IGBINARY, // SERIALIZER_NONE, SERIALIZER_PHP, SERIALIZER_IGBINARY
 		'prefix'     => NULL
 	),
 );
